7a076d8d3e124697fc5628c8d73c986e8c8b93df
bu23 <70064197+bu23@users.noreply.github.com>
2020-09-29 21:29:39 +0900

    記述誤り修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,10 +6,11 @@ class PostsController < ApplicationController
 
   def create
     Post.create(content: params[:content])
-    redirect_to action: :index 
+    redirect_to action: :index
   end
 
   def checked
+    binding.pry
     post = Post.find(params[:id])
     if post.checked 
       post.update(checked: false)
